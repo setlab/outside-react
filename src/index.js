6cf1512cdf670870123d17969a682522ec2ebd7c
setlab <lab@setlab.net>
2023-02-23 20:31:30 +0200

    react integration

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={window.integrationData} />
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
